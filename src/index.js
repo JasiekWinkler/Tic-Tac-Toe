137ba654e37f5582c5c0bc3f18bc5dd63c1e7037
Jan <jasiek.winkler@gmail.com>
2020-09-28 22:20:10 +0200

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Game from './components/Game.js';
+import './index.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Game />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
